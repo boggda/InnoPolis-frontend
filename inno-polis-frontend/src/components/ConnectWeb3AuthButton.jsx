041d5c7d1cc2ca67e3dfaa7c8ebb5347bedc8dfa
Louie-ru <mnr1908@yandex.ru>
2024-11-15 23:59:41 +0700

    ADD test function and web3 provider

--- inno-polis-frontend/src/components/ConnectWeb3AuthButton.jsx
+++ inno-polis-frontend/src/components/ConnectWeb3AuthButton.jsx
@@ -2,20 +2,39 @@ import React from 'react';
 import { useWeb3Auth } from "@web3auth/modal-react-hooks";
 
 const ConnectWeb3AuthButton = () => {
-  const { isConnected, connect } = useWeb3Auth();
-
+  const { isConnected, connect, addAndSwitchChain, userInfo, provider, web3Auth, authenticateUser } = useWeb3Auth();
+  
   if (isConnected) {
-    return null;
+    return (
+      <div
+        className="flex flex-row rounded-full px-6 py-3 text-white justify-center align-center cursor-pointer"
+        style={{ backgroundColor: "green" }}
+        onClick={test}
+      >
+        Test
+      </div>
+    );
   }
+
+  function test(){
+    console.log(provider);
+  }
+
+  /*
+  function connect_wrapper(){
+    let provider = connect();
+    console.log(provider);
+  }
+  */
   
   return (
-    <div
-      className="flex flex-row rounded-full px-6 py-3 text-white justify-center align-center cursor-pointer"
-      style={{ backgroundColor: "#0364ff" }}
-      onClick={connect}
-    >
-      Connect to Web3Auth
-    </div>
+      <div
+        className="flex flex-row rounded-full px-6 py-3 text-white justify-center align-center cursor-pointer"
+        style={{ backgroundColor: "#0364ff" }}
+        onClick={connect}
+      >
+        Connect to Web3Auth
+      </div>
   );
 };
 
