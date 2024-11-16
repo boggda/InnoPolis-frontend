7be8a6db6e3b647e1da44a791528c8eaca9ac9b5
boggda <black_sec@protonmail.com>
2024-11-17 00:09:25 +0700

    add authmanagers

--- inno-polis-frontend/src/config/const.js
+++ inno-polis-frontend/src/config/const.js
@@ -1,2 +1,2 @@
 //export const factoryAddr = "0x00c1e00a0a48AF8388716aa7b1765400Be97be5d";
-export const factoryAddr = "0x0b512d63ab93ad49aa99d2e1f184ef9922622a79";
+export const factoryAddr = "0xc10e58A47624d9275B5D3De5b4e3dECD1658Fb50";
--- inno-polis-frontend/src/services/topicService.js
+++ inno-polis-frontend/src/services/topicService.js
@@ -11,22 +11,36 @@ export const addTopicService = async (newTopic, newDescription, topics, provider
     const fromAddress = (await web3.eth.getAccounts())[0];
     const contract = new web3.eth.Contract(conversationFactoryABI, factoryAddr);
 
+    let calls = [];
     // Determine the auth manager address based on the selected type
     let authManager = "0x0000000000000000000000000000000000000000";
     if (authType !== 'none') {
-      // You'll need to implement the logic to get the correct auth manager contract
-      // based on the authType, authAddress, and minValue
-      //authManager = determineAuthManager(authType, authAddress, minValue);
+      if (authType === 'ens') {
+        calls.push(contract.methods.createENSHoldingManager(authAddress).encodeABI());
+        authManager = await contract.methods.createENSHoldingManager(authAddress).call();
+      } else if (authType === 'chiliz') {
+        calls.push(contract.methods.createTokenHoldingManager(authAddress, minValue).encodeABI());
+        authManager = await contract.methods.createTokenHoldingManager(authAddress, minValue).call();
+      } else if (authType === 'erc20') {
+        calls.push(contract.methods.createTokenHoldingManager(authAddress, minValue).encodeABI());
+        authManager = await contract.methods.createTokenHoldingManager(authAddress, minValue).call();
+      } else if (authType === 'eth') {
+        calls.push(contract.methods.createETHBalanceManager(minValue).encodeABI());
+        authManager = await contract.methods.createETHBalanceManager(minValue).call();
+      }
     }
     console.log(authType, authAddress, minValue);
+    console.log(authManager);
 
     const title = newTopic.trim();
     const description = newDescription.trim();
     const duration = 60*60*24*3;
 
+    calls.push(contract.methods.createConversation(title, description, authManager, duration).encodeABI());
+
     // Encode the function call
     return new Promise((resolve, reject) => {
-      contract.methods.createConversation(title, description, authManager, duration)
+      contract.methods.multicall(calls)
         .send({from: fromAddress, value: 0})
         .on('transactionHash', (hash) => {
           console.log('Transaction Hash:', hash);
