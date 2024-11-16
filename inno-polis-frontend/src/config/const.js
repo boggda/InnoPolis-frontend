0e92e50e26dfe0f8b6fd69920b938bd79a8677f3
frodan <fronc2000@gmail.com>
2024-11-17 05:24:20 +0700

    + hostname

--- inno-polis-frontend/src/config/const.js
+++ inno-polis-frontend/src/config/const.js
@@ -4,4 +4,4 @@ export const factoryAddrList = {
     84532: "0xc10e58A47624d9275B5D3De5b4e3dECD1658Fb50"
 };
 
-export const backendUrl = "http://localhost";
+export const backendUrl = "http://209.145.59.143";
