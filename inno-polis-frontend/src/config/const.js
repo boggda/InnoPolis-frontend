2a00c283e46655afce224ae52f11c0c68761b421
Alex <posikera@bk.ru>
2024-11-17 07:14:28 +0700

    + factory addresses

--- inno-polis-frontend/src/config/const.js
+++ inno-polis-frontend/src/config/const.js
@@ -1,7 +1,11 @@
 //export const factoryAddrList = "0x00c1e00a0a48AF8388716aa7b1765400Be97be5d";
 export const factoryAddrList = {
     17000: "0x00c1e00a0a48AF8388716aa7b1765400Be97be5d",
-    84532: "0xc10e58A47624d9275B5D3De5b4e3dECD1658Fb50"
+    84532: "0xc10e58A47624d9275B5D3De5b4e3dECD1658Fb50",
+    80002: "0x44EaF4Ee7Fc4e2787E1c23757515231cd2A5cae8", // polygon testnet
+    42220: "0x44EaF4Ee7Fc4e2787E1c23757515231cd2A5cae8", // celo testnet
+    11155420: "0xde823356390084d7E8F4104caf008Ac3917459E2", // optimism testnet
+    59141: "0xde823356390084d7E8F4104caf008Ac3917459E2" // linea sepolia
 };
 
 export const backendUrl = "http://localhost";
